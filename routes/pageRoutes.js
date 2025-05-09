d62fa78bf354eee97561fe0b93a8ebe963f61d88
MarkusR22 <markusreblin22@gmail.com>
2025-05-09 19:50:59 +0200

    Added basic routing

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,19 +3,33 @@
 const express = require('express');
 const router = express.Router();
 
+const teamMembers = [];
+const events = [];
+
 router.get('/', (req, res) => {
+    res.render('pages/home', {events})
 });
 
 router.get('/about', (req, res) => {
+    res.render('pages/about', {teamMembers})
 });
 
 router.get('/events', (req, res) => {
+    res.render('pages/events', {events})
 });
 
 router.get('/contact', (req, res) => {
+    res.render('pages/contact')
 });
 
+router.post('/contact', (req, res) => {
+    const {name, email, message} = req.body;
+
+    res.redirect('/thankyou')
+})
+
 router.get('/thankyou', (req, res) => {
+    res.render('pages/thankyou')
 });
 
 module.exports = router;
